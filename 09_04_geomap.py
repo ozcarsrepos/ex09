57703c0043c33fb42b6942a10a9949ddfe8779e9
ozcar <ozcarnguyen@gmail.com>
2019-05-10 16:00:34 +0700

    Listing N places nearby a location

---
+++ 09_04_geomap.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import sys  # NOQA
+import argparse
+import logging  # NOQA
+import requests
+import json
+import time
+from geojsonio import display
+# logger = log.get_logger(__name__)
+logger = logging.getLogger(__name__)    # NOQA
+logger.setLevel(logging.DEBUG)  # NOQA
+
+
+# Make feature geojson data
+def feature(obj):
+    result = {"type": "Feature"}
+    result["geometry"] = {"type": "Point"}
+    result["geometry"]["coordinates"] = [obj['geometry']['location']['lat'],
+                                         obj['geometry']['location']['lng']]
+    result["properties"] = dict()
+    result["properties"]["name"] = obj['name']
+    result["properties"]["Address"] = obj['vicinity']
+    result["rating"] = obj['rating']
+    return result
+
+
+# Get result from googlemaps with num & locate, default locate = 'DHGTVT',
+# default radius = 2000 meters, keyword is optional in command line
+def geo_result(num=50, **locate):
+    '''locate.setdefault("location", "10.8048982,106.7170081")
+    locate.setdefault("radius", "2000")
+    locate.setdefault("type", "restaurant")'''
+    locate.setdefault("key", "AIzaSyCLYmd6U1YQhHPDJn9jL8ihulOc4yUmuzg")
+
+    googlemaps_api_url = (
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json')
+    ses = requests.Session()
+    req = ses.get(googlemaps_api_url, params=locate)
+    resp = req.json()
+    # Establishing GeoJSON data
+    geo_data = {"type": "FeatureCollection", "features": []}
+    n = min(num, 20)    # each result page (pagesize) has 20 records
+    for rs in resp['results'][:n]:
+        geo_data["features"].append(feature(rs))
+    num -= 20
+    while num > 0:
+        time.sleep(2)
+        locate_next = dict()
+        locate_next['pagetoken'] = resp['next_page_token']
+        locate_next['key'] = "AIzaSyCLYmd6U1YQhHPDJn9jL8ihulOc4yUmuzg"
+        req = ses.get(googlemaps_api_url, params=locate_next)
+        resp = req.json()
+        n = min(num, 20)
+        for rs in resp['results'][:n]:
+            geo_data["features"].append(feature(rs))
+        num -= 20
+
+    return geo_data
+
+
+def main():
+    if len(sys.argv) == 1:
+        geojson_data = geo_result()
+    else:
+        try:
+            parser = argparse.ArgumentParser(description=(
+                'display a type (default is restaurants) map in radius '
+                '(default is 2000 m) of a location (default is Dai hoc '
+                'giao thong van tai - duong Vo Oanh, Q. Binh Thanh)'))
+            parser.add_argument('--lat', '-l', default=10.8048982, type=float,
+                                help='input the latitude of your location')
+            parser.add_argument('--lng', '-g', default=106.7170081, type=float,
+                                help='input the longtitude of your location')
+            parser.add_argument('--type', '-t', default='restaurant',
+                                dest='maptype', help='type of your map default'
+                                ' is restaurant')
+            parser.add_argument('--numpage', '-n', default=50, type=int,
+                                dest='num', help='input the pagesize, '
+                                'default is 50')
+            parser.add_argument('--radius_map', '-r', default=2000, type=int,
+                                dest='mapradius', help='input the radius of '
+                                'your searching (default is 2000 m)')
+            args = parser.parse_args()
+            locate = dict()
+            locate["location"] = [args.lat, args.lng]
+            locate["type"] = args.maptype
+            locate["radius"] = args.mapradius
+            geojson_data = geo_result(args.num, locate)
+        except Exception as e:
+            logger.debug('Appear exception:', e)
+
+    # Output to a file (JSON serialization)
+    with open('pymi_beer.geojson', 'w', encoding='utf8') as f:
+        json.dump(geojson_data, f, indent=2)
+
+    # Convert to a JSON string and display on geojson.io
+    display(json.dumps(geojson_data))
+
+
+if __name__ == '__main__':
+    main()
